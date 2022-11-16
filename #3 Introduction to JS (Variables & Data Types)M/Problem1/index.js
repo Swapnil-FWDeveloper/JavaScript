9bda0a838fdedb9cd9a39102834eace3dc55620f
Swapnil Shende <116657863+Swapnil-FWDeveloper@users.noreply.github.com>
2022-11-16 23:59:28 +0530

    Add files via upload

---
+++ #3 Introduction to JS (Variables & Data Types)M/Problem1/index.js
@@ -0,0 +1,6 @@
+// console.log("Masai School")
+// console.log("A Transformation in Education")
+let Instute="Masai School";
+let Moral='A Transformation in Education';
+console.log(Instute)
+console.log(Moral)
