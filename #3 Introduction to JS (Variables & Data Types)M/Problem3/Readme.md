8a3b64efecb41d99d50bbd9a3a238dc17b039f58
Swapnil Shende <116657863+Swapnil-FWDeveloper@users.noreply.github.com>
2022-11-16 23:58:25 +0530

    Add files via upload

---
+++ #3 Introduction to JS (Variables & Data Types)M/Problem3/Readme.md
@@ -0,0 +1,1 @@
+Replet link:-https://replit.com/@SwapnilShende1/VariablesDay-1#problem_three.js
---
+++ #3 Introduction to JS (Variables & Data Types)M/Problem3/index.js
@@ -0,0 +1,4 @@
+let UserName='Swapnil Shende';
+let UserAge=20;
+console.log(UserName,UserAge)
+console.log(typeof(UserName),typeof(UserAge))
