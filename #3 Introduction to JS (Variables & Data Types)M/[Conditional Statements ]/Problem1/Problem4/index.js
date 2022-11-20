ace276cbc8117aa53597b75b7cbd0e0dfc4d8d21
Swapnil Shende <116657863+Swapnil-FWDeveloper@users.noreply.github.com>
2022-11-21 01:59:16 +0530

    Add files via upload

---
+++ #3 Introduction to JS (Variables & Data Types)M/[Conditional Statements ]/Problem1/Problem4/index.js
@@ -0,0 +1,11 @@
+
+
+let username ='Swapnil Shende';
+let password ="!@#$%^&*()";
+if(username==password)
+{
+  console.log("User can Login")
+}else if(username!=password)
+{
+  console.log("User Can't Login")
+}
