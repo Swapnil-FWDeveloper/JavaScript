a7d3454a7a47a52fa6a4e57f8427cdc450a0619e
Swapnil Shende <116657863+Swapnil-FWDeveloper@users.noreply.github.com>
2022-11-21 01:57:22 +0530

    Add files via upload

---
+++ #3 Introduction to JS (Variables & Data Types)M/[Conditional Statements ]/Problem1/Problem3/index.js
@@ -0,0 +1,14 @@
+
+
+let a=5;
+let b=10;
+if(a>b)
+{
+  console.log("a is greater than b")
+}else if(b>a)
+{
+  console.log("b is greater than a")
+}else if(a==b)
+{
+  console.log("both are equal")
+}
