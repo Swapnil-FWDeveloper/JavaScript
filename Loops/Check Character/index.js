825ce3fb5e8431a6540dc16ecc66c921d6482e72
Swapnil Shende <116657863+Swapnil-FWDeveloper@users.noreply.github.com>
2022-12-06 23:40:49 +0530

    character is a small case or not

---
+++ Loops/Check Character/index.js
@@ -0,0 +1,17 @@
+
+let lower='abcdefghijklmnopqrstuvwxyz'
+let upper='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+let char='A'
+let flag=false
+for(let i=0;i<lower.length;i++){
+  if(char[i]==lower[i])
+  {
+    flag=true  
+    break;
+  }
+}
+if(flag == true){
+  console.log('It is Capital letter')
+}else{
+  console.log('It is not a Capital letter')
+}
